81667bc25beabb4e502812942fdc69e5a6de505b
Maggie <50422221+mbagg4152@users.noreply.github.com>
2021-09-25 14:01:42 -0400

    m

--- BigPlantMoment/Assets/LoadMain.cs
+++ BigPlantMoment/Assets/LoadMain.cs
@@ -3,7 +3,16 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 public class LoadMain : MonoBehaviour {
-   public void OnButtonPress(){
-       SceneManager.LoadScene("MainScene");
-   }
+//    public void OnButtonPress(){
+//        
+//    }
+    void Start() {}
+
+
+    void Update(){
+        if (Input.GetKey(KeyCode.JoystickButton0) || Input.GetKey(KeyCode.Mouse0)) { // allow right trigger or mouse click
+            SceneManager.LoadScene("MainScene");       
+        }
+    }
+
 }
